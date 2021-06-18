96fec6c2d2abbe60b9357630fc2ab8ec37dcefbb
Jinkeloid <377844252@qq.com>
2021-06-18 21:00:53 +0800

    Credits Scene

--- core/src/main/java/com/jinkeloid/mispd/ui/Window.java
+++ core/src/main/java/com/jinkeloid/mispd/ui/Window.java
@@ -49,6 +49,7 @@ public class Window extends Group implements Signal.Listener<KeyEvent> {
 	public static final int WHITE = 0xFFFFFF;
 	public static final int TITLE_COLOR = 0xFFFF44;
 	public static final int SHPX_COLOR = 0x33BB33;
+	public static final int MISPD_COLOR = 0x30B7FF;
 	
 	public Window() {
 		this( 0, 0, 0, Chrome.get( Chrome.Type.WINDOW ) );
